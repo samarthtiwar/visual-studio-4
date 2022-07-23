3bc713fc24cb5107c40c2f87091a0f3239ce2197
samarthtiwar <109853989+samarthtiwar@users.noreply.github.com>
2022-07-23 16:43:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,5 @@
+console.log("my name is samarth tiwari");
+console.log("my school name is raja ram mohan roy academy");
+console.log("i am 11 years old");
+console.log("i study in class 7");
+console.log("my father name is hemant tiwari");
